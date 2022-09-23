20b025057d1688453021070b53c94ff0d88c61b7
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2022-09-23 13:34:22 +0530

    Time: 4 ms (17.40%), Space: 41.6 MB (39.15%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.java
+++ 7-reverse-integer/7-reverse-integer.java
@@ -1,6 +1,7 @@
 class Solution {
     public int reverse(int n) {
         
+        // for negative values
         int flag = n<0 ? -1:1;
         n = n*flag;
         
@@ -8,16 +9,14 @@ class Solution {
              while(n>0){
                   st.push(n%10);
                    n/=10;
-              }
-               
-               
-         long r=0;
-         long i=1;
+              }      
+         long r=0, i=1;      
         while(!st.empty()){
             r = r + st.pop() * i; 
             i*=10;
-        }
+        } 
         
+        // for over 32bit, i.e. long type
         if(r<=Integer.MIN_VALUE || r>=Integer.MAX_VALUE)
             return 0;
             
