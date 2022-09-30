b34c8679fef401501c0277604c63f6e0527d9e75
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2022-09-30 22:47:58 +0530

    Time: 18 ms (38.07%), Space: 63.7 MB (81.22%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.java
@@ -4,14 +4,18 @@ class Solution {
       // if the number at position i-1 is already negative, i is the number that occurs twice.
         
        List list = new ArrayList<Integer>();
+        int n = arr.length;
         
-        for(int i = 0; i < arr.length; i++) {            
-            int index = Math.abs(arr[i])-1;            
-            if (arr[index] < 0)
-                list.add(Math.abs(index+1));
+        for(int i = 0;i<n;i++){
+            int val = Math.abs(arr[i]);
             
-            arr[index] = -arr[index];
+            //logic for checking val is duplicate or not
+            if(arr[val - 1] < 0) list.add(val);
+            
+            //if val is not duplicate , mark it visited now
+            else arr[val - 1] *= - 1;
         }
+        
         return list;
                  
     }
