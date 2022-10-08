873a422c5896104e3c090fb9108ddc9083804d6d
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2022-10-08 10:04:10 +0530

    Time: 6 ms (98.87%), Space: 43.3 MB (91.62%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -2,32 +2,42 @@ class Solution {
     public List<Integer> findAnagrams(String s, String p) {
        
         List ans = new ArrayList();
-        HashMap<Character,Integer> mp = new HashMap<>();
-        HashMap<Character,Integer> ms = new HashMap<>();
+        int[] mp = new int[26];
+        int[] ms = new int[26];
         
          int slen = s.length();
-        int plen = p.length();
-        if(plen>slen) return ans;       
+        int window = p.length();
+        if(window>slen) return ans;       
         
-        int l=0,r=0;
-       while(r<plen){          
-            mp.put(p.charAt(r),mp.getOrDefault(p.charAt(r),0)+1);
-            ms.put(s.charAt(r),ms.getOrDefault(s.charAt(r++),0)+1);
+        int l=0,r=-1;
+        while(++r<window){          
+            mp[p.charAt(r)-'a']++;
+            ms[s.charAt(r)-'a']++;
         } 
-        if(mp.equals(ms))  ans.add(l);          
-     
-        while(r<slen)  { 
-            ms.put(s.charAt(r),ms.getOrDefault(s.charAt(r++),0)+1);
-            
-            char c = s.charAt(l++);
-            ms.put(c,ms.get(c)-1);  
-            if(ms.get(c)==0)
-                ms.remove(c);
-           
-             if(mp.equals(ms))  ans.add(l);            
+        
+        if(isEqual(mp,ms))   
+            ans.add(l);     
           
+     
+        while(r<slen)  {          
+            ms[s.charAt(r++)-'a']++;
+                       
+            
+            ms[s.charAt(l++)-'a']--;             
+             
+            
+            if(isEqual(mp,ms))   
+                ans.add(l);           
         }
         return ans;
         
+    }
+    
+    private boolean isEqual(int[] mp,int[] ms){
+        for(int i=0;i<26; i++){
+            if(mp[i]!=ms[i])
+                return false;
+        }
+        return true;
     }
 }
