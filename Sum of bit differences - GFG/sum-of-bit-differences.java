d74e4ec2d80908ed929126c7416d52b43bd3a953
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2022-10-30 18:52:00 +0530

    Added solution - LeetHub

---
+++ Sum of bit differences - GFG/sum-of-bit-differences.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            String[] inputLine = br.readLine().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            System.out.println(new Solution().sumBitDifferences(arr, n));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    long sumBitDifferences(int[] arr, int n) {
+        // code here
+         long ans=0;
+                for(int i=31; i>=0; i--){
+                        int c1=0, c0=0;
+                        for(int j=0; j<n; j++){
+                                if((arr[j]&(1<<i))>0){
+                                        c1++;
+                                }else
+                                        c0++;
+                        }
+                        ans += (long) c1*c0;
+                }
+        return ans*2;
+    }
+}
