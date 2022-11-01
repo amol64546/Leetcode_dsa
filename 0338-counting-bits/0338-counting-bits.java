340a1039670f46e4fa1d176b9358c8629c29a820
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2022-11-01 18:01:10 +0530

    Time: 1 ms (99.96%), Space: 46.3 MB (92.58%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -2,18 +2,10 @@ class Solution {
     public int[] countBits(int n) {
         int[] ans = new int[n+1];
         for(int i=1; i<=n; i++){
-            ans[i] = helper(i);
+            ans[i] = ans[i/2] + (i&1);
         }
         return ans;
     }
     
-    int helper(int n){
-        // if(n==0) return 0;
-        if(n==1) return 1;
-        
-        if((n&1)==0) return helper(n/2);
-        return 1+helper(n/2);
-                 
-        
-    }
+   
 }
