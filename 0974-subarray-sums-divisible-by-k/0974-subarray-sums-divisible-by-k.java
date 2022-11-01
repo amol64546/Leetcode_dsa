638dc47213d074f66f5f320772f7309dc67cdd51
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2022-11-01 20:30:34 +0530

    Time: 22 ms (73.76%), Space: 54.9 MB (72.33%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -2,12 +2,16 @@ class Solution {
     public int subarraysDivByK(int[] arr, int k) {
         
        int[] remFreq = new int[k];
-        remFreq[0] = 1;
-        int sum = 0, count = 0;
+        
+        int sum = 0,rem=0, count = 0;
+         remFreq[0] = 1;
         for (int i : arr) {
-            sum = (sum + i % k + k) % k;
-            count += remFreq[sum]++;
+            sum += i%k+k;
+            rem = sum%k;  
+            
+            count += remFreq[rem]++;
         }
+        
         return count;
              
     }
