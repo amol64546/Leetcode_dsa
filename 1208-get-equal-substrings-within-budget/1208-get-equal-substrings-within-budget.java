6fc1ef054c57be4c083aabd169b032f6fe4f7dd5
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2022-11-03 09:39:36 +0530

    Time: 6 ms (93.94%), Space: 42.3 MB (90.99%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
@@ -3,16 +3,20 @@ class Solution {
         
        int n = s.length();
         int start=0;
-        int ans = 0;
-        for(int i=0; i<n; i++){
+        int ans = 0, i;
+        for( i=0; i<n; i++){
             maxCost -= Math.abs(s.charAt(i)-t.charAt(i));
+            // System.out.print(maxCost+" ");
             if(maxCost<0){
                 maxCost += Math.abs(s.charAt(start)-t.charAt(start));
                 start++;
             }
-            ans = Math.max(ans,i-start+1);
+            // System.out.println(maxCost+" ");
+            // ans = Math.max(ans,i-start+1);
+            // System.out.println(i+" "+start);
         }
+        // System.out.println(i+" "+start);
         
-        return ans;
+        return i-start;
     }
 }
