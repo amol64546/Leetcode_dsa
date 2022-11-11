4c9e517a7f434a7eed037db6a34718c206cfad8a
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2022-11-11 16:48:12 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (7.01%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,23 +1,25 @@
 class Solution {
+    public void swapQuad(int[][] matrix, int i, int j,int n){
+        
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[n-j-1][i];
+        matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
+        matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
+         matrix[j][n-i-1] = temp;
+        
+        
+    }
+    
+    
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-        
-        // transpose
+       
         for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+            for(int j=i; j<n-i-1; j++){
+                swapQuad(matrix,i,j,n);
             }
         }
         
-        // swap col
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n/2; j++){
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][n-j-1];
-                matrix[i][n-j-1] = temp;
-            }
-        }
+        
     }
 }
