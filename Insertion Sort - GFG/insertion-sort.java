9e917b67a5ca61cdb363f222821a940e010555f8
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2022-11-19 09:59:21 +0530

    Added solution - LeetHub

--- Insertion Sort - GFG/insertion-sort.java
+++ Insertion Sort - GFG/insertion-sort.java
@@ -48,12 +48,11 @@ class Solution
      
       for(int i=1; i<n; i++){
           int temp = arr[i];
-          int j=i;
-          while(j>0 && temp<arr[j-1]){
-                arr[j] = arr[j-1];
-                j--;
+          int j=i-1;
+          while(j>=0 && temp<arr[j]){
+                arr[j+1] = arr[j--];
           }
-          arr[j] = temp;
+          arr[j+1] = temp;
       }
       
       
