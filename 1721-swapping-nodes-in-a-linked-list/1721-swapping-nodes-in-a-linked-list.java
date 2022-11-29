8e9aac9b553a50b7981fa63cf7df7a446e1e8dcb
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2022-11-29 17:39:20 +0530

    Time: 4 ms (63.94%), Space: 164.2 MB (72.20%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -10,51 +10,37 @@
  */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-        int n=0;
-        ListNode temp=head;
-        while(temp!=null){
+          int s=k;
+        int n = 0;
+        ListNode curr=head;
+        while(curr!=null){
             n++;
-            temp = temp.next;
+            curr=curr.next;
         }
-        
-        // if kth node from start and end is same
-        if(k==n-k+1) return head;
-       
-        
-        ListNode curr1=head, curr2=head;
-        ListNode prev1=null, prev2=null;        
-        
-        // reaching at kth node from start
-        for(int i=1; i<k; i++){
-            prev1=curr1;
-            curr1 = curr1.next;
+        int e=n-k+1;                         
+        if(s==e)  return head;    // both nodes are same
+    ListNode dummy = new ListNode(0);
+	dummy.next = head;
+	
+     // traverse
+        ListNode prev1=dummy, curr1=head;
+        while(s-->1){
+          prev1=curr1;
+          curr1=curr1.next;
+        }       
+        ListNode prev2=dummy, curr2=head;
+        while(e-->1){
+            prev2=curr2;
+            curr2=curr2.next;
         }
-        // reaching at kth node from end
-        for(int i=1; i<n-k+1; i++){
-             prev2=curr2;
-            curr2 = curr2.next;
-        }
-        
-        // swapping
-        if(prev1!=null){
-            prev1.next = curr2;
-        }else{
-            head = curr2;
-        }
-        
-        if(prev2!=null){
-            prev2.next = curr1;
-        }else{
-            head = curr1;
-        }
-        
-        temp = curr1.next;
+		
+         // swap		
+        prev1.next = curr2;  		
+        prev2.next = curr1;                            
+        ListNode temp = curr1.next;
         curr1.next = curr2.next;
         curr2.next = temp;
-        
-        return head;
-        
-        
-        
+		
+		return dummy.next;
     }
 }
