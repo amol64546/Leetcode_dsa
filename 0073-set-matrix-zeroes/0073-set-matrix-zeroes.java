8741323a9b79985bb216bae3b700c224d94f48ef
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2022-12-08 10:49:04 +0530

    Time: 2 ms (60.54%), Space: 54.4 MB (18.60%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,31 +2,56 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int r = matrix.length;
         int c = matrix[0].length;
-        int[] rowZ = new int[r];
-        int[] colZ = new int[c];
-        Arrays.fill(rowZ,1);
-        Arrays.fill(colZ,1);
         
-        
-        
+        boolean isRowZ = false;
+        boolean isColZ = false;
+    
+    // checking zeroes
+        // first row
         for(int i=0; i<r; i++){
-            for(int j=0; j<c; j++){
+            if(matrix[i][0]==0)
+                isRowZ = true;
+        }
+        
+        // first col
+        for(int j=0; j<c; j++){
+            if(matrix[0][j]==0)
+                isColZ = true;
+        }
+        
+        
+        // excluding first row and first col        
+        for(int i=1; i<r; i++){
+            for(int j=1; j<c; j++){
                 if(matrix[i][j]==0){
-                   rowZ[i] = 0;
-                   colZ[j] = 0;
+                   matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
         
-        System.out.println(Arrays.toString(rowZ));
-         System.out.println(Arrays.toString(colZ));
-        for(int i=0; i<r; i++){
-            for(int j=0; j<c; j++){
-                if(rowZ[i]==0 || colZ[j]==0){
+   // setting zeroes
+        // excluding first row and first col    
+        for(int i=1; i<r; i++){
+            for(int j=1; j<c; j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
                     matrix[i][j] = 0;
                     
                 }
             }
         }
+        
+        // first row
+        for(int i=0; i<r; i++){
+            if(isRowZ)                 
+                matrix[i][0]=0;
+        }
+        
+        // first col
+        for(int j=0; j<c; j++){
+            if(isColZ)                 
+                matrix[0][j]=0;
+        }
+        
     }
 }
