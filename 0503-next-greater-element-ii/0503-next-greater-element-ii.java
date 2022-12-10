530e132d4cefdacad3e865c36f3dc9d36ddcf7cf
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2022-12-10 20:22:01 +0530

    Time: 43 ms (48.34%), Space: 54.6 MB (69.23%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -3,14 +3,24 @@ class Solution {
         int n = nums.length;
         var st = new Stack<Integer>();
         int[] res = new int[n];
-        Arrays.fill(res,-1);
-        for(int i=0; i< 2*n; i++){
-            int curr = nums[i%n];
-            while(!st.empty() && curr>nums[st.peek()])
-                res[st.pop()] = curr;
-            if(i<n)
-                st.push(i);
+       
+                
+        for(int i=n-1; i>=0;  i--)
+            st.push(nums[i]);
+            
+        for(int i=n-1; i>=0;  i--){    
+                int curr = nums[i];
+                 while(!st.empty() && curr>=st.peek()){
+                          st.pop();                        
+                 }
+                if(st.empty())
+                   res[i] = -1;                                                        
+                else //if(curr<st.peek())
+                        res[i] = st.peek();    
+                
+                st.push(curr); 
         }
+                
         return res;
     }
 }
