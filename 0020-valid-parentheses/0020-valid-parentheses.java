3a7ca35c8a4b874131ee163d3d9b8806a4ade6cc
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2022-12-10 12:30:13 +0530

    Time: 4 ms (56.85%), Space: 42.2 MB (39.20%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,30 +1,18 @@
 class Solution {
-    public boolean isValid(String s) {
-        
-        Stack<Character> st = new Stack<>();
-        
-        for(int i=0; i<s.length(); i++){
-            char c = s.charAt(i);
-            if(c=='(' || c=='{' || c=='['){
-                st.push(c);
-            }else{
-                if(!st.empty() && isValid(st.peek(),c)){
-                    st.pop();
-                }else{
-                    return false;
+    public boolean isValid(String str) {
+           Stack<Character> st = new Stack<>();
+                 for(int i=0; i<str.length(); i++){
+                        char c = str.charAt(i);
+                        if(c=='(' || c=='{' || c=='[' ){
+                                st.push(c);
+                        }else{
+                            if(st.empty() || Math.abs(c-st.pop())>2) 
+                                return false;
+                        }
                 }
-            }
-        }
+               
         return st.empty();
     }
     
-    public boolean isValid(char open,char close){
-        if(open=='(' && close==')')
-            return true;
-        if(open=='[' && close==']')
-            return true;
-        if(open=='{' && close=='}')
-            return true;
-        return false;
-    }
+     
 }
