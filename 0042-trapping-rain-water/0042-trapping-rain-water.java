07fff84f9b1aef23b70edc6dc91add53bbe736b8
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2022-12-12 20:46:38 +0530

    Time: 3 ms (37.39%), Space: 48.3 MB (70.18%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,24 +1,22 @@
 class Solution {
-    public int trap(int[] heights) {
-        int n = heights.length;        
+    public int trap(int[] height) {
+        int n = height.length;
+        
         if(n<3) return 0;        
        
-        var st = new Stack<Integer>();
-        int volume =0;
-        
-        for(int i=0; i<n; i++){
-            while(!st.empty() && heights[i]>heights[st.peek()]){
-                int index = st.pop();
-                           
-            if(!st.empty()){
-              int height = Math.min(heights[i],heights[st.peek()]) -heights[index];
-                int width  = i - st.peek() -1;
-                volume += (height * width);
-              }   
-            }
-            st.push(i);
+        int[] rightMax = new int[n];                        
+        rightMax[n-1] = height[n-1];
+        for(int i=n-2; i>=0; i--){
+            rightMax[i] = Math.max(height[i], rightMax[i+1]);
         }
         
-        return volume;
+        int sum=0;
+        int leftMax = height[0];
+        for(int i=0; i<n; i++){
+            leftMax = Math.max(leftMax,height[i]);
+            sum+= Math.min(leftMax,rightMax[i])-height[i];
+        }
+        
+        return sum;
     }
 }
