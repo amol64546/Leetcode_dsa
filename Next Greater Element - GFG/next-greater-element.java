02b66c95ffabf73e55be85ddb52f9a505b254a7d
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2022-12-12 19:06:47 +0530

    Added solution - LeetHub

--- Next Greater Element - GFG/next-greater-element.java
+++ Next Greater Element - GFG/next-greater-element.java
@@ -32,20 +32,19 @@ class GFG {
 class Solution
 {
     //Function to find the next greater element for each element of the array.
-    public static long[] nextLargerElement(long[] arr, int n)
+    public static long[] nextLargerElement(long[] nums, int n)
     { 
-        // Your code here
-        Stack<Long> st = new Stack<>();
-        long[] ans = new long[n];
-        for(int i=n-1; i>=0; i--){
-            while(!st.empty() && st.peek()<=arr[i])
-                st.pop();
-            if(st.empty())
-                ans[i]=-1;
-            else // st.peek()>arr[i]
-                ans[i]=st.peek();
-            st.push(arr[i]);
+       
+        var st = new Stack<Integer>();
+        long[] res = new long[n];
+        Arrays.fill(res,-1);
+        
+        for(int i=0; i< n; i++){
+            while(!st.empty() && nums[i]>nums[st.peek()])
+                res[st.pop()] = nums[i];
+            st.push(i);
         }
-        return ans;
-    } 
+        return res;
+    }
 }
+    
