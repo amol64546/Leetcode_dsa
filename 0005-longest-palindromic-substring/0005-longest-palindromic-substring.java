1efec0ea315793719fdc4643468880f3cfe19e27
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2022-12-18 13:11:52 +0530

    Time: 34 ms (66.93%), Space: 52.2 MB (30.29%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,31 +1,26 @@
 class Solution {
-    public String longestPalindrome(String s) {
-        int n = s.length();
-        if(n==1) return s;
-        String ans = "";
-        int maxLen=0;        
-        for(int i=0; i<n; i++){
-            // odd  length         
-                int l=i, r=i;                
-                while(l>=0 && r<n && s.charAt(l)==s.charAt(r)){
+    String ans = "";
+    int maxLen=0;
+
+    public void extendPalindrome(String s, int l, int r){       
+                while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
                     int len=r-l+1;
                     if(len>maxLen){
                         ans = s.substring(l,r+1);
                         maxLen = len;
                     }
                     l--; r++;
-                }                        
-            
-            // even length          
-                l=i; r=i+1;                 
-                while(l>=0 && r<n && s.charAt(l)==s.charAt(r)){
-                    int len=r-l+1;
-                    if(len>maxLen){
-                        ans = s.substring(l,r+1);
-                        maxLen = len;                    }
-                    l--; r++;
-                }
-            
+                } 
+    }
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        if(n==1) return s;
+        for(int i=0; i<n; i++){
+            // odd                                             
+            extendPalindrome(s,i,i);
+            // even  
+            extendPalindrome(s,i,i+1);  
+      
         }
         return ans;
     }
