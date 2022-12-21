de5269bac85226c302740681cb4691208567408f
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2022-12-21 13:49:30 +0530

    Time: 19 ms (14.81%), Space: 63 MB (17.40%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -3,33 +3,44 @@ class Solution {
     int perimeter=0;
     public int islandPerimeter(int[][] grid) {
          int sr = grid.length, sc = grid[0].length;
-       
+       boolean[][] visited = new boolean[sr][sc];
         for(int i=0; i<sr; i++){
             for(int j=0; j<sc; j++){
-                if(grid[i][j]==0) continue;
-                count(grid,i+1,j);
-                count(grid,i-1,j);
-                count(grid,i,j+1);
-                count(grid,i,j-1);
+                if(grid[i][j]==1){
+                    DFS(grid,i,j,visited);
+                    break;
+                }
+               
             }
         }
         return perimeter;
     }
 
       
-     public void count(int[][] grid, int sr, int sc){
+     public void DFS(int[][] grid, int sr, int sc, boolean[][] visited){
+       
          
         if(sr<0 || sr>=grid.length){
-            perimeter += 1;
+            perimeter++;
             return;
-           
         }                     
         if(sc<0 || sc>=grid[0].length){
-             perimeter += 1;
+             perimeter++;
             return;
-        }           
-        if(grid[sr][sc] == 0)
-            perimeter += 1;       
+        }
+         
+        if(grid[sr][sc]==0){   //  water 
+            perimeter++;
+            return;
+        }
+        if(visited[sr][sc]) return;   // visited
+         
+         visited[sr][sc] = true;
+               
+         DFS(grid,sr+1,sc,visited);
+         DFS(grid,sr-1,sc,visited);
+         DFS(grid,sr,sc+1,visited);
+         DFS(grid,sr,sc-1,visited);
             
     }
     
