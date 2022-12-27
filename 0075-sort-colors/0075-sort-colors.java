6d1eca9dd055d44a818ecc7cb1bce5f60fb4c1c4
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2022-12-27 14:41:39 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (90.13%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,41 +1,15 @@
 class Solution {
-    public void print(int[] arr){
-        for(int i: arr){
-            System.out.print(i+" ");
-        }
-        System.out.println();
-    } 
+ 
     public void sortColors(int[] nums) {
-        print(nums);
-        int l=0, r=nums.length-1;
+         int l=0, r=nums.length-1;
         int i=0;
-        while(i<=r){
-           if(nums[i]==0){              
-               int temp = nums[i];
-                nums[i] = nums[l];
-                nums[l] = temp;                
-               l++;
-               
-               // sure it will be 1 at i after swapping
-             
-               print(nums);
-           }
-           
-           if(nums[i]==2){                
-                int temp = nums[i];
-                nums[i] = nums[r];
-                 nums[r] = temp;              
-               r--;  
-               i--;
-               print(nums);
-                // not sure which will be at i after swapping
-           }
-           
-           i++;
-         
-           
+        while(i<=r){                           
+            if(nums[i]==0)             
+                nums[i] = (nums[i]+nums[l]) - (nums[l++]=nums[i++]) ; 
+            else if(nums[i]==2)
+                nums[i] = (nums[i]+nums[r]) - (nums[r--]=nums[i]) ; 
+            else i++;                 
         }
-        print(nums);
     }
 }
     
