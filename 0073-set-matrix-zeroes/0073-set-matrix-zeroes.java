fc4a53e60eb85251a3eaaa4fbfe9c1164c7bfac4
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2022-12-30 14:32:40 +0530

    Time: 1 ms (93.52%), Space: 43.9 MB (85.22%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -19,35 +19,35 @@ class Solution {
                 isColZ = true;
         }
         
+     // setting zeroes  
         
-        // excluding first row and first col        
         for(int i=1; i<r; i++){
             for(int j=1; j<c; j++){
-                if(matrix[i][j]==0){
-                   matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+                if(matrix[i][j]==0){  // inner mat
+                   matrix[i][0] = 0;  // set first col
+                    matrix[0][j] = 0;  // set first row
                 }
             }
         }
         
-   // setting zeroes
-        // excluding first row and first col    
+   
+        
         for(int i=1; i<r; i++){
             for(int j=1; j<c; j++){
-                if(matrix[i][0]==0 || matrix[0][j]==0){
-                    matrix[i][j] = 0;
+                if(matrix[i][0]==0 || matrix[0][j]==0){  // first row and col
+                    matrix[i][j] = 0;   // set inner mat
                     
                 }
             }
         }
         
-        // first row
+        // first row acc to starting iRowZ
         for(int i=0; i<r; i++){
             if(isRowZ)                 
                 matrix[i][0]=0;
         }
         
-        // first col
+        //  first col acc to starting isColZ
         for(int j=0; j<c; j++){
             if(isColZ)                 
                 matrix[0][j]=0;
