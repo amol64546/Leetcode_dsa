65c3c71770f4861d732e87d6e50cd6e4a5daa21b
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2023-01-06 20:09:54 +0530

    Time: 29 ms (15.01%), Space: 47.2 MB (56.81%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,46 +1,44 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         
-        // building graph
-        List<List<Integer>> graph = new ArrayList<>();
+       
         
-        for(int i=0; i<numCourses; i++){
-            graph.add(new ArrayList<Integer>());
-        }
         
         // dealing with dependencies
         int[] dep = new int[numCourses];
         
-        for(int[] course: prerequisites){
-            graph.get(course[1]).add(course[0]);
+        for(int[] course: prerequisites){           
             dep[course[0]]++;
         }
         
         // startin dfs
         Stack<Integer> st = new Stack<>();
-        
+        int index=0;
         for(int i=0; i<numCourses; i++){
             if(dep[i]==0){
                  st.push(i);
+                index++;
             }
         }
         
         while(!st.empty()){
             int curr = st.pop();
-            List<Integer> list = graph.get(curr);
-            for(int nbr: list){                
-                dep[nbr]--;                
-                if(dep[nbr]==0){
-                    st.push(nbr);
+            
+            for(int i=0; i<prerequisites.length; i++){    
+                int src = prerequisites[i][1];
+                int nbr = prerequisites[i][0];
+                
+                if(src==curr){
+                     dep[nbr]--;                
+                    if(dep[nbr]==0){
+                        st.push(nbr);
+                        index++;
+                    }
                 }
             }            
         }
-        for(int i=0; i<numCourses; i++){
-            if(dep[i]>0){
-                 return false;
-            }
-        }
-        return true;
+        
+        return (index==numCourses) ? true:false;
        
     }
 }
