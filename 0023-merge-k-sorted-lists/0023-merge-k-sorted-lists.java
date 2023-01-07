4afd9e16ebe0e7e1960d36e2d4835f7abcd9ff62
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2023-01-07 12:52:30 +0530

    Time: 1 ms (100.00%), Space: 44.3 MB (72.95%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,34 +9,42 @@
  * }
  */
 class Solution {
-    class Pair{
-        ListNode node;
-        int val;
-        Pair(ListNode node, int val){
-            this.node = node;
-            this.val = val;
-        }
-    }
     public ListNode mergeKLists(ListNode[] lists) {
-        
-        PriorityQueue<Pair> min = new PriorityQueue<>((a,b)-> a.val - b.val );
+        int n = lists.length;
+        if(n==0) return null;
+        return mergeSort(lists,0,n-1);
+    }
+    
+    public ListNode mergeSort(ListNode[] lists, int s,int e){
+        if(s==e) return lists[s];
+        int mid = (s+e)/2;
+        ListNode left = mergeSort(lists,s,mid);
+        ListNode right = mergeSort(lists,mid+1,e);
+        return merge(left,right);
+    }
+    
+    public ListNode merge(ListNode l1,ListNode l2){
+        if(l1==null) return l2;
+        if(l2==null) return l1;
         
         ListNode dummy = new ListNode();
         ListNode temp = dummy;
         
-        for(int i=0; i<lists.length; i++){
-            if(lists[i] != null)
-                min.offer(new Pair(lists[i],lists[i].val));
+        while(l1!=null && l2!=null){
+            if(l1.val<l2.val){
+                temp.next = l1;
+                l1 = l1.next;
             }
-        
-        while(!min.isEmpty()){
-            Pair curr = min.poll();
-            temp.next = curr.node;
+            else{
+                temp.next = l2;
+                l2 = l2.next;
+            }
             temp = temp.next;
-            if(temp.next != null){
-                min.offer(new Pair(temp.next,temp.next.val));
-            }
         }
+        
+        temp.next = (l1!=null) ? l1 : l2;
         return dummy.next;
+        
+        
     }
 }
