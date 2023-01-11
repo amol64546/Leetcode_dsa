bbb97baedb8b0f1b8c49bb9bed46b2a92fb5217d
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2023-01-11 13:28:26 +0530

    Added solution - LeetHub

--- Quick Sort - GFG/quick-sort.java
+++ Quick Sort - GFG/quick-sort.java
@@ -36,23 +36,37 @@ class Solution
     //Function to sort an array using quick sort algorithm.
     static void quickSort(int arr[], int low, int high)
     {
-        if(low>=high) return;
+        // base case
+        if(low >= high) return;
+        // getting partition index
         int pi = partition(arr,low,high);
+        
+        // sort elements before partition
         quickSort(arr,low,pi-1);
+        // sort elements after partition
         quickSort(arr,pi+1,high);
     }
+    
+    // select last ele as pivot
+    // places pivot at its correct pos acc sorted array
+    // places all smaller in left and greater in right of pivot
+    
     static int partition(int arr[], int l, int r)
     {
-         int pivot = arr[r];
-        int i=l;
+        int pivot = arr[r];
+        int i = l;
+        
         for(int j=l; j<=r; j++){
-            if(arr[j]<=pivot){
-                int temp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = temp;
-                i++;  
+            // swapping smaller than pivot to left part
+            if(arr[j] <= pivot){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
             }
         }
+        // pivot index
         return i-1;
+        
     } 
 }
