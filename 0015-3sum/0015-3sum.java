b0180a44a5fe06b8f2f4ffdb108a4bfa46df5402
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2023-01-29 10:50:42 +0530

    Time: 41 ms (51.54%), Space: 49.3 MB (53.49%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,33 +1,31 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] arr) {
-        int n = arr.length;
-        Arrays.sort(arr);
-        List ans= new ArrayList();
-        
-        for(int i=0; i<n-2; i++){  
-            if(i>0 && arr[i]==arr[i-1]) continue;
-           int j=i+1;
-            int k=n-1;
-            while(j<k){
-                
-                int target=-(arr[j]+arr[k]);
-                if(arr[i]>target)
-                    k--;
-                else if(arr[i]<target)
-                    j++;
-                else{                                      
-                    ans.add(Arrays.asList(arr[i],arr[j],arr[k]));
-                    
-                    // update both till we get equal elements
-                    while(j<k && arr[j]==arr[j+1])
-                        j++;                    
-                    while(j<k && arr[k]==arr[k-1])
-                        k--;
-                    j++;
-                    k--;                    
-                }                                                                               
-            }            
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> mainList = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length-2; i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            twoSum(nums,i,mainList);
         }
-        return ans;
+        return mainList;
+    }
+     
+     public void twoSum(int[] numbers, int i, List<List<Integer>> mainList) {
+        int l=i+1, r=numbers.length-1;
+        
+        while(l<r){
+            int sum = numbers[i]+numbers[l]+numbers[r];
+            if(sum == 0){
+                mainList.add(Arrays.asList(numbers[i],numbers[l],numbers[r]));
+                while(l<r && numbers[l]==numbers[l+1]) l++;
+                while(l<r && numbers[r]==numbers[r-1]) r--;
+                l++;
+                r--;
+            }else if(sum < 0){
+                l++;
+            }else{
+                r--;
+            }
+        }
+        
     }
 }
