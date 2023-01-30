39d3e16b127b8299cd101fb29f5906eeb8706b27
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2023-01-30 22:57:56 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (98.32%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,14 +1,20 @@
 class Solution {
    
     public int uniquePathsWithObstacles(int[][] a) {
-        int[][] dp = new int[a.length][a[0].length];
-        return explore(a,0,0,dp);
+        int m = a.length, n = a[0].length;
+        int[][] dp = new int[m+1][n+1];
+        
+        
+        for(int i=m-1; i>=0; i--){
+            for(int j=n-1; j>=0; j--){
+                if(a[i][j]==1) continue; // obstacle
+                if(i==m-1 && j==n-1) dp[i][j] = 1;
+                else dp[i][j] = dp[i+1][j]+dp[i][j+1];
+            }
+        }
+        //for(int[] arr:dp) System.out.println(Arrays.toString(arr));
+        return dp[0][0];
     }
     
-    public int explore(int[][] a,int i,int j,int[][] dp){
-        if(i==a.length || j==a[0].length || a[i][j]==1) return 0;
-        if(i+1==a.length && j+1==a[0].length) return 1;
-        if(dp[i][j]!=0) return dp[i][j];
-        return dp[i][j] = explore(a,i+1,j,dp)+explore(a,i,j+1,dp);
-    }
+   
 }
