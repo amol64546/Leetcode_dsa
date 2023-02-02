e97b2a9d9e55d5e896638ed98a73e5a5c1516c0f
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2023-02-02 13:58:10 +0530

    Added solution - LeetHub

---
+++ Swap and Maximize - GFG/swap-and-maximize.java
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+
+
+class GFG
+{
+    long maxSum(long arr[] ,int n)
+    {
+    
+        Arrays.sort(arr);
+        int i=0, j=n-1;
+        long[] newArr = new long[n];
+        int k=0;
+        while(i<=j){
+            if(k%2==1)
+                newArr[k++] = arr[i++];
+            else
+                newArr[k++] = arr[j--];
+        }
+        
+        long ans = 0;
+        for(int p=0; p<n-1; p++){
+            ans += Math.abs(newArr[p]-newArr[p+1]);
+        }
+        ans += Math.abs(newArr[0]-newArr[n-1]);
+        
+        return ans;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main (String[] args) throws IOException{
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while(testcases-- > 0){
+            int n=Integer.parseInt(br.readLine());
+//            String line = br.readLine();
+//            String[] a2 = line.trim().split("\\s+");
+//            int n =Integer.parseInt(a2[0]);
+//            int k =Integer.parseInt(a2[1]);
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            long arr[]=new long[n];
+            for(int  i=0;i<n;i++)
+            {
+                arr[i]=Long.parseLong(a1[i]);
+            }
+            GFG ob=new GFG();
+            //ArrayList<Long> ans=ob.smallestDifferenceTriplet(a,b,c,n);
+            long ans=ob.maxSum(arr,n);
+            System.out.println(ans);
+        }
+    }
+}
+
+
+
+
+// } Driver Code Ends
