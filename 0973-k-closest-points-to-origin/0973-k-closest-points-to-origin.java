5642411600296d0576894aea477bec42354dfd67
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2023-02-03 11:37:08 +0530

    Time: 46 ms (44.94%), Space: 50.7 MB (59.24%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,32 +1,24 @@
 class Solution {
-    public int[][] kClosest(int[][] points, int k) {
-        int n = points.length;
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->b.dist-a.dist);
+    public int[][] kClosest(int[][] p, int k) {
+        int n = p.length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> dist(p[b])-dist(p[a]) );
         
-        for(int i=0; i<n; i++){
-             int x = points[i][0], y = points[i][1];
-             int dist = x*x + y*y;
-            pq.add(new Pair(dist,x,y));
+        for(int i=0; i<n; i++){            
+            pq.add(i);
             if(pq.size()>k) pq.remove();
         }
         
        
         int[][] ans = new int[k][2];
-        for(int i=0; i<k; i++){
-            Pair p = pq.remove();
-            int x = p.x, y = p.y;
-            ans[i][0] = x;
-            ans[i][1] = y;
+        for(int i=0; i<k; i++){     
+            int idx = pq.remove();
+            ans[i] = p[idx];          
         }
         return ans;
     }
-}
-
-class Pair{
-    int x,y,dist;
-    Pair(int dist,int x,int y){
-        this.dist=dist;
-        this.x = x;
-        this.y = y;
+    
+    public int dist(int[] p){
+        return p[0]*p[0] + p[1]*p[1];
     }
 }
+
