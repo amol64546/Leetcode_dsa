fc7b30777821a4b2dbb634f095f0c5cc4bbf2eb7
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2023-02-05 19:59:20 +0530

    Time: 6 ms (73.80%), Space: 42.6 MB (53.49%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -2,25 +2,27 @@ class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> set = new HashSet<>(wordDict);
         int[] dp = new int[s.length()];
-       // return recur(s,0,set,dp);
-       return tabulation(s,set);
+       return recur(s,0,set,dp);
+       // return tabulation(s,set);
     }
     
      boolean recur(String s,int pos,Set<String> set,int[] dp ){
 		int len = s.length();
-        if (pos >= len) {
+        if (pos == len) {
             return true;
         }
         if(dp[pos] != 0) return dp[pos]==1? true:false;
         for(int i=pos; i<len; i++){
             if(set.contains(s.substring(pos,i+1)) && recur(s,i+1,set,dp)){
-                    dp[pos] = 1;
+                    dp[pos] = 1; //true
                     return true;
             }                
         }
-        dp[pos] = -1;
+        dp[pos] = -1; //false
        return false;
 	}
+    
+    
     
     boolean tabulation(String s,Set<String> set){
         int n = s.length();
