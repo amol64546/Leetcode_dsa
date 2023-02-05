f25f8c56864a5a5f973b34cb27e5d3465600ff4c
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2023-02-05 19:55:40 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (56.39%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -8,7 +8,7 @@ class Solution {
         for(int i=m-1; i>=0; i--){
             for(int j=n-1; j>=0; j--){
                 if(a[i][j]==1) continue; // obstacle
-                if(i==m-1 && j==n-1) dp[i][j] = 1;
+                if(i==m-1 && j==n-1) dp[i][j] = 1;  //destination
                 else dp[i][j] = dp[i+1][j]+dp[i][j+1];
             }
         }
