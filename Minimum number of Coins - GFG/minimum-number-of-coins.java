bade1cd9eb7e3d36e0dd049541836bd7ea05c48e
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2023-02-05 15:33:06 +0530

    Added solution - LeetHub

--- Minimum number of Coins - GFG/minimum-number-of-coins.java
+++ Minimum number of Coins - GFG/minimum-number-of-coins.java
@@ -28,53 +28,22 @@ class GFG{
 // User function Template for Java
 
 class Solution{
-    static List<Integer> minPartition(int n)
+    static List<Integer> minPartition(int amount)
     {
-       List<Integer> list = new ArrayList<Integer>();
-       while(n>0){
-           if(n>=2000){
-               list.add(2000);
-               n -= 2000;
-           }
-           else if(n>=500){
-               list.add(500);
-               n -= 500;
-           }
-           else if(n>=200){
-               list.add(200);
-               n -= 200;
-           }
-           else if(n>=100){
-               list.add(100);
-               n -= 100;
-           }
-           else if(n>=50){
-               list.add(50);
-               n -= 50;
-           }
-           else if(n>=20){
-               list.add(20);
-               n -= 20;
-           }
-           else if(n>=10){
-               list.add(10);
-               n -= 10;
-           }
-           else if(n>=5){
-               list.add(5);
-               n -= 5;
-           }
-           else if(n>=2){
-               list.add(2);
-               n -= 2;
-           }
-           else if(n>=1){
-               list.add(1);
-               n -= 1;
-           }
-           
-       }
-       return list;
+        // code here
+        int[] coins = { 1, 2, 5, 10, 20, 50, 100, 200, 500, 2000 } ;
+       
+        List<Integer> list = new ArrayList<>();
         
+        for(int i=coins.length-1; i>=0; i--){
+            while(amount>=coins[i]){
+                amount -= coins[i];
+                list.add(coins[i]);
+            }
+        }
+        return list;
     }
+    
+    
+    
 }
