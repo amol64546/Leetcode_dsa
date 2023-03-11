f07fdf98fd3f30e35b2aed3ebe2b474398b455aa
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2023-03-11 13:15:31 +0530

    Time: 1 ms (99.98%), Space: 50.3 MB (86.88%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,27 +1,16 @@
 class Solution {
-    public int firstMissingPositive(int[] nums) {
-        int n = nums.length;
+    public int firstMissingPositive(int[] arr) {
+        int n = arr.length;
+        
+        int[] map = new int[n+1];
         
         for(int i=0;i<n;i++){
-            if(nums[i]<0)
-                nums[i]=0;
+            if(1<=arr[i] && arr[i]<=n)
+                map[arr[i]]++;
         }
-        
-        for(int i=0;i<n;i++){
-             int val = Math.abs(nums[i]);
-            if(val>=1 && val<=n){               
-                if(nums[val-1]>0){
-                    nums[val-1] *= -1;
-                }else if(nums[val-1]==0){
-                    nums[val-1] = -1 * (n+1);
-                }
-            }
-        }
-        
-        for(int i=1; i<=n; i++){
-            if(nums[i-1]>=0){
+        for(int i=1;i<=n;i++){
+            if(map[i]==0)
                 return i;
-            }
         }
         return n+1;
     }
