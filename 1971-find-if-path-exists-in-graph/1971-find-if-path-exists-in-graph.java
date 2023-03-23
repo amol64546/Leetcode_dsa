8951de8cd108886e4fc3ad8a53752aaf599e74ca
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2023-03-23 10:10:58 +0530

    Time: 134 ms (55.60%), Space: 151.4 MB (41.13%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,36 +1,32 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        
-        List<List<Integer>> graph = new ArrayList<>();  
-        for (int i = 0; i < n; i++){
-            graph.add(new ArrayList<Integer>());
-        }      
-	
-        for (int i = 0; i < edges.length; i++){
-            int u = edges[i][0];   
-            int v = edges[i][1];           
-            graph.get(u).add(v);   
-            graph.get(v).add(u);                    
+           // make a adj list        
+        List<List<Integer>> adj = new ArrayList<>();        
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
         }
+        for(int[] e: edges){
+            adj.get(e[0]).add(e[1]);
+            adj.get(e[1]).add(e[0]);
+        }
+        boolean[] vis = new boolean[n];
+        if(dfs(adj,source,destination,vis)) return true;
+        return false;
+    }
+    
+    public boolean dfs( List<List<Integer>> adj,int s,int d, boolean[] vis){
+        if(vis[s]) return false;
+        vis[s]= true;
         
-
-        boolean[] visited = new boolean[n];        
-        Queue<Integer> pq = new LinkedList<>();
-		pq.offer(source);
-	 
-	  while(!pq.isEmpty()){
-		  int curr = pq.poll();	  		  
-		  if(curr==destination) return true; 
-          if(visited[curr]) continue;	
-          visited[curr] = true; 	     
-
-		  List<Integer> adj = graph.get(curr);      
-		  for(int nbr: adj){
-			  if(!visited[nbr]){                                    
-				  pq.offer(nbr);
-			  }
-		  }	  
-	  }
-	  return false;
-	}
+        if(s==d) return true;
+        
+        for(int nbr: adj.get(s)){
+            if(dfs(adj,nbr,d,vis)) return true;
+            
+        }
+        return false;
+    }
+    
+    
+    
 }
